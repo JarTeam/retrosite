61912f565ae86928445abed12a27e11cb9de1429
JarHead <jarek.rosol@icloud.com>
2021-07-20 13:27:48 -0400

    thumbs.db

---
+++ thumbsdbdelete.bat
@@ -0,0 +1,10 @@
+@echo off
+color 1
+title thumbsdbdelete
+echo JAR STOP LEAVING BEHIND THUMBS.DB AHJDHSJAHDNHUAHJN
+echo This file will delete every thumbs.db.
+pause
+del thumbs.db /a /s /f
+echo Success! All thumbs.db should now be in file hell where they belong.
+echo help he's gonna break my thumbs -jarhead
+pause
